7f34c3b03202d5fc84c422c2c9e557504fc82ecb
Nika M. <merabishvilinikusha@gmail.com>
2024-05-26 20:51:00 +0400

    Add Props

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import equilibriumImage from "./images/image-equilibrium.jpg";
-import Eth from "./Eth";
-import Avatar from "./Avatar";
+import etheriumImage from "./images/icon-ethereum.svg";
+import clockImage from "./images/icon-clock.svg";
+import avatarPicture from "./images/image-avatar.png";
 
 function Card() {
   return (
@@ -12,8 +13,31 @@ function Card() {
       <p style={{ color: "var(--primary-soft-blue)", paddingTop: 12 }}>
         Our Equilibrium collection promotes balance and calm.
       </p>
-      <Eth />
+      <Eth eth="0.041" days={3} />
       <Avatar />
+    </div>
+  );
+}
+
+function Eth(props) {
+  return (
+    <div
+      style={{
+        paddingTop: 16,
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ display: "flex", gap: "6.48px", alignItems: "center" }}>
+        <img src={etheriumImage} alt="etherium" />
+        <span style={{ color: "var(--primary-cyan)" }}>{props.eth} ETH</span>
+      </div>
+      <div style={{ display: "flex", gap: "6.48px", alignItems: "center" }}>
+        <img src={clockImage} alt="Clock" />
+        <span style={{ color: "var(--primary-soft-blue)" }}>
+          {props.days} days left
+        </span>
+      </div>
     </div>
   );
 }
@@ -30,5 +54,34 @@ const styles = {
     margin: "0 auto",
   },
 };
+
+function Avatar() {
+  return (
+    <div>
+      <div
+        style={{
+          height: "1px",
+          width: "100%",
+          backgroundColor: "var(--neutral-very-dark-blue-line)",
+          margin: "16px 0",
+        }}
+      ></div>
+      <div className="flex-center" style={{ gap: "16px" }}>
+        <img
+          style={{ border: "1px solid white", borderRadius: 50 }}
+          src={avatarPicture}
+          height={"33px"}
+        />
+        <span style={{ color: "var(--primary-soft-blue)" }}>
+          Creation of <span style={{ color: "white" }}>{returnName()}</span>
+        </span>
+      </div>
+    </div>
+  );
+
+  function returnName() {
+    return "Jules Wyvern";
+  }
+}
 
 export default Card;
